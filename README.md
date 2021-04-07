3dfeb929be1191ddc5ce54badbfe32d2c6a37061
bgallahue@mycervello.com <bgallahue@mycervello.com>
2021-04-06 22:05:23 -0400

    assignments and component

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Repo for Evolve Front End Code Reviews
+4/7/2021 - Review
+Component: buildSinglePricesComponent
+Assignments:
+
+HTML/CSS
+All Lines: 
+David Manhardt
+Artem Hamzin
+
+Javascript 
+Lines 1-90
+Alex Popov
+Anton Kozhemyakin
+
+Javascript 
+Lines 91 - 183
+Ilya Toloknov
+Bobby Gallahue
---
+++ force-app/main/default/lwc/buildSinglePricesComponent/buildSinglePricesComponent.js
@@ -0,0 +1,183 @@
+/**
+ * Created by Artem Hamzin on 13.03.2020.
+ */
+
+import {LightningElement, track, api, wire} from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getLastPricesModificationDate from "@salesforce/apex/SmartRatesEnrollmentController.getLastPricesModificationDate";
+import getInitialStatus from "@salesforce/apex/SmartRatesEnrollmentController.getInitialStatus";
+import startBuildingPrices from "@salesforce/apex/SmartRatesEnrollmentController.startBuildingPrices";
+import getCurrentJobStatus from '@salesforce/apex/SmartRatesEnrollmentController.getCurrentJobStatus';
+import TIME_ZONE from '@salesforce/i18n/timeZone';
+import { DateTime } from "c/luxon";
+
+const COMPLETED_STATUSES = ['Completed'];
+const IN_PROCESS_STATUSES = ['Pending', 'In Queue', 'In Progress', 'Moved to Daily Job', 'Holding',  'Queued', 'Preparing', 'Processing', 'Single Listing Update Prices Job has been started.'];
+const FAILED_STATUSES = ['Error', 'Aborted', 'Failed'];
+
+export default class BuildSinglePricesComponent extends  NavigationMixin(LightningElement)  {
+    @api listing_id;
+    @api user_id;
+
+    refresher = 0;
+
+    loadingMessage = "Loading Component";
+    lastPricesUpdateDate = "";
+
+    status = "Press \"Build Prices\" button to start process."; //display job status
+    statusMessage = ""; //full info
+    startBtnDisabled = false;
+
+    jobStartedAt = null;
+
+    inProgress = true;
+
+    $ = {
+        inited: false
+    }
+
+    updateTimer = {
+        timer: null,
+        timeoutSeconds: 10000
+    }
+
+    connectedCallback() {
+        if (this.$.inited) return;
+        this.getLastPricesModificationDate();
+        this.getInitialStatus();
+        this.$.inited = true;
+
+    }
+
+    //
+    // methods
+    //
+
+    @wire(getCurrentJobStatus, { listingId: '$listing_id', jobName: 'Single Listing Build Prices', jobStartTime: '$jobStartedAt', refresher: '$refresher'  })
+    wiresJobStatus({ error, data }) {
+        if (data) {
+            if (this.refresher > 0){
+                this.status = data;
+                this.statusMessage = '';
+                if (COMPLETED_STATUSES.includes(data) || FAILED_STATUSES.includes(data)){
+                    this.inProgress = false;
+                    this.startBtnDisabled = false;
+                    clearInterval(this.updateTimer.timer);
+                    this.refresher = 0;
+                }
+            }
+        } else if (error) {
+            console.log('WiredJobError -> ',error);
+            this.statusMessage = error.body.message;
+            this.status = 'Failed';
+        }
+    };
+
+    buildPricesStart(){
+        this.statusMessage = "";
+        this.startBuildingPrices();
+        this.startBtnDisabled = true;
+    }
+
+    backToListing(){
+        window.history.back();
+    }
+
+    getLastPricesModificationDate() {
+        getLastPricesModificationDate({
+            listingId: this.listing_id
+        })
+            .then(result => {
+                console.debug('GetLastPricesModificationDateResult -> ', result);
+                this.lastPricesUpdateDate = result;
+            })
+            .catch(error => {
+                console.log('GetLastPricesModificationDateError -> ', error);
+            });
+    }
+
+    getInitialStatus(){
+        getInitialStatus({
+            listingId: this.listing_id,
+            jobName: 'Single Listing Build Prices'
+        })
+            .then(result => {
+                console.debug('GetInitialStatusResult -> ', result);
+
+                if (result !== 'Ready'){
+                    //this.statusMessage = result ? result : 'Unknown error';
+                    this.startBtnDisabled = true;
+
+                    this.updateStatus();
+                    this.status = result;
+                }
+            })
+            .catch(error => {
+                console.log('GetInitialStatusError -> ', error);
+                this.statusMessage = error.body.message;
+                this.status = 'Failed';
+                this.startBtnDisabled = true;
+            }).finally(() => {
+            this.inProgress = false;
+        })
+    }
+
+    startBuildingPrices() {
+        startBuildingPrices({
+            listingId: this.listing_id
+        })
+            .then(result => {
+                console.debug('StartBuildingPricesResult -> ', result);
+                this.status = result;
+                this.jobStartedAt = DateTime.fromMillis(new Date().getTime(), {zone: TIME_ZONE}).toMillis();
+                this.updateStatus();
+                if (result !== 'Single Listing Update Prices Job has been started.'){
+                    this.status = 'Failed';
+                }
+            })
+            .catch(error => {
+                console.log('StartBuildingPricesError -> ', error);
+                this.statusMessage = error.body.message;
+                this.status = 'Failed';
+            })
+            .finally(() => {
+                this.inProgress = false;
+            })
+    }
+
+    updateStatus() {
+        this.updateTimer.timer = setInterval( () => {
+            this.refresher++;
+            // this.getCurrentJobStatus();
+        }, this.updateTimer.timeoutSeconds);
+    }
+
+    //
+    // getters
+    //
+
+    get LastPricesModificationDate(){
+        return this.lastPricesUpdateDate;
+    }
+
+    get isStartBtnDisabled(){
+        return this.startBtnDisabled;
+    }
+
+    get isInProgress(){
+        return this.inProgress;
+    }
+    get statusClass(){
+        console.log('###')
+        if (COMPLETED_STATUSES.includes(this.status) ){
+            return "slds-box slds-p-vertical_large slds-theme_alert-texture status-display status-display_succeeded";
+        }
+        if (FAILED_STATUSES.includes(this.status)){
+            return "slds-box slds-p-vertical_large slds-theme_alert-texture status-display status-display_failed";
+        }
+        if (IN_PROCESS_STATUSES.includes(this.status)){
+            return "slds-box slds-p-vertical_large slds-theme_alert-texture status-display status-display_info animation-texture";
+        }
+        return "slds-box slds-p-vertical_large slds-theme_alert-texture status-display ";
+    }
+}
